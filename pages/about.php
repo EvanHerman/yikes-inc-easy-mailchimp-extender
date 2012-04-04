083fd7ad41500090d08412cd8f7243e82cd452fb
Tracy Levesque <tracy@yikesinc.com>
2012-04-04 18:00:44 -0400

    I updated the About page and css.

--- pages/about.php
+++ pages/about.php
@@ -6,16 +6,32 @@
 	</h2>
 
 	<div id="yksme-page-content">
+		<div id="yksme-yikes-logo-container">
 		<a href="http://www.yikesinc.com" title="YIKES, Inc." target="_blank"><img src="<?php echo YKSEME_URL; ?>/images/yikes_logo.png" alt="YIKES, Inc." id="yksme-yikes-logo" /></a>
-		<h3>YIKES, Inc. Web Design and Development</h3>
-		<h4>Bringing Ideas To Life Since 1996</h4>
 		
-		<p><a href="http://www.yikesinc.com" target="_blank">YIKES, Inc.</a> is a Philadelphia company dedicated to&nbsp;<strong>sustainable business practices</strong>. Since 1996 YIKES has continued to be a socially responsible business observing the&nbsp;<strong>triple bottom line</strong>: people, planet, profit. YIKES is committed to building a more socially, environmentally and financially sustainable local economy.</p>
-		<p>YIKES is proud to have earned&nbsp;<strong><a href="http://www.bcorporation.net/" target="_blank">B Corporation Certification</a></strong>&nbsp;having successfully completed B Corp's screening process, and is a member and sponsor of the&nbsp;<a href="http://www.sbnphiladelphia.org/" target="_blank"><strong>Sustainable Business Network of Greater Philadelphia</strong></a>.</p>
-		<p>The YIKES office is located in a green (soon to be LEED certified) building and is powered by&nbsp;<strong>100% Renewable electricity</strong>&nbsp;through&nbsp;<a href="http://www.theenergyco-op.com/" target="_blank"><strong>The Energy Cooperative</strong></a>. YIKES recycles and composts office waste and prints promotional materials on recycled paper with soy ink. The YIKES office uses green cleaning and paper products. YIKES is also a&nbsp;<a href="http://www.replanttrees.org/" target="_blank"><strong>100% Replanted</strong></a>&nbsp;office.</p>
-		<p>YIKES was honored to receive the&nbsp;<strong>2011 B Corporation Award "Most Inspirational Project</strong>," and was a recipient of a&nbsp;<a href="http://www.sbnphiladelphia.org/events/svi2005folder/triplebottomlineawards" target="_blank"><strong>Triple Bottom Line Award</strong></a>&nbsp;at the 2005&nbsp;<a href="http://www.sbnphiladelphia.org/events/svi2005folder/" target="_blank"><strong>Social Venture Institute</strong></a>&nbsp;in the service category.</p>
-		<p>YIKES is a&nbsp;<strong>certified Woman Owned Business Enterprise</strong>&nbsp;by the Commonwealth of Pennsylvania Bureau of Minority and Women Business Opportunities and City of Philadelphia Minority Business Enterprise Council now known as the Office of Economic Opportunity.</p>
-		<p><a href="http://www.yikesinc.com/people.php" title="YIKES Team" target="_blank">YIKES employees</a> donate a percentage of YIKES' profits to non-profit organizations every year.</p>
+			<div class="b-sbn">
+			<a href="http://www.bcorporation.net/yikes" target="_blank"><img src="<?php echo YKSEME_URL; ?>/images/bcorp.jpg" alt="Certified B Corporation"></a>
+			<a href="http://www.sbnphiladelphia.org/" target="_blank"><img src="<?php echo YKSEME_URL; ?>/images/sbn_logo.png" alt="YA proud sponsor of the Sustainable Business Network of Philadelphia"></a>
+			</div>
+		
+		<a href="http://facebook.com/yikesinc" target="_blank"><img src="<?php echo YKSEME_URL; ?>/images/facebook.png" style="border: 0px none;" alt="YIKES Philadelphia Web design and Development Facebook" height="24" width="24"></a> <a href="http://twitter.com/yikesinc" target="_blank"><img src="<?php echo YKSEME_URL; ?>/images/twitter.png" style="border: 0px none;" alt="YIKES Philadelphia Web design and Development Twitter" height="24" width="24"></a> <script gapi_processed="true" type="text/javascript" src="http://apis.google.com/js/plusone.js"></script>
+		<div style="height: 24px; width: 106px; display: inline-block; text-indent: 0pt; margin: 0pt; padding: 0pt; background: none repeat scroll 0% 0% transparent; border-style: none; float: none; line-height: normal; font-size: 1px; vertical-align: baseline;" id="___plusone_0"><iframe allowtransparency="true" hspace="0" id="I1_1333575714870" marginheight="0" marginwidth="0" name="I1_1333575714870" src="https://plusone.google.com/_/+1/fastbutton?url=http%3A%2F%2Fwww.yikesinc.com%2F&amp;size=standard&amp;count=true&amp;hl=en-US&amp;jsh=m%3B%2F_%2Fapps-static%2F_%2Fjs%2Fgapi%2F__features__%2Frt%3Dj%2Fver%3DkJTTGFio1Bk.en.%2Fsv%3D1%2Fam%3D%21Ze6NnRS0VYCICGRMrA%2Fd%3D1%2Frs%3DAItRSTO0675vsQCIIVLyCqE1jWrDTqqpjw#id=I1_1333575714870&amp;parent=http%3A%2F%2Fwww.yikesinc.com&amp;rpctoken=371844436&amp;_methods=onPlusOne%2C_ready%2C_close%2C_open%2C_resizeMe%2C_renderstart" style="position: static; left: 0pt; top: 0pt; width: 106px; margin: 0px; border-style: none; height: 24px; visibility: visible;" tabindex="0" vspace="0" title="+1" frameborder="0" scrolling="no" width="100%"></iframe></div><br>
+		 <a href="http://yikesinc.tumblr.com/" target="_blank"><img src="<?php echo YKSEME_URL; ?>/images/tumblr.png" style="border: 0px none;" alt="YIKES Philadelphia Web design and Development Tumblr" height="24" width="24"></a> 
+		<a href="http://www.linkedin.com/companies/yikes-inc" title="Linkedin" target="_blank"><img src="<?php echo YKSEME_URL; ?>/images/linkedin.png" alt="YIKES Philadelphia Web design and Development Linkedin" border="0" height="24" width="24"></a>
+		<a href="http://www.flickr.com/photos/yikesinc/" target="_blank"><img src="<?php echo YKSEME_URL; ?>/images/flickr.png" style="border: 0px none;" alt="YIKES Philadelphia Web design and Development Flickr" height="24" width="24"></a>
+		<a href="http://www.youtube.com/yikesinc/" target="_blank"><img src="<?php echo YKSEME_URL; ?>/images/youtube.png" style="border: 0px none;" alt="YIKES Philadelphia Web design and Development YouTube" height="24" width="24"></a>
+		<a href="http://pinterest.com/yikesinc/" target="_blank"><img src="<?php echo YKSEME_URL; ?>/images/PinterestIcon.png" style="border: 0px none;" alt="YIKES Philadelphia Web design and Development Pinterest" height="24" width="24"></a> 
+		</div>
+		
+		<h3>YIKES, Inc. Web Design and Development</h3>
+		
+		<h4>Smart, effective, beautifully designed web solutions for the Greater Philadelphia region and beyond. </h4>
+		
+		<p>Since 1996 YIKES, Inc. has built creative web design and development solutions for businesses and non-profit organizations. We specialize in creating custom WordPress themes and plugins.</p>
+		
+		<p>In addition to making great WordPress sites YIKES is also committed to being a socially responsible business observing the triple bottom line: people, planet, profit. We work to help build a more socially, environmentally and financially sustainable local economy.</p>
+		
+		<p>Learn more about us at <a href="http://www.yikesinc.com" title="YIKES, Inc." target="_blank">www.yikesinc.com</a></p>
 	</div>
 	
 </div>
