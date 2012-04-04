e32ba3a62d901a6f178970c3e9a0260dd26b6a50
Carlos Z <carlos@zinniasol.com>
2012-04-04 13:27:43 -0400

    change contact info and version number

--- yikes-inc-easy-mailchimp-extender.php
+++ yikes-inc-easy-mailchimp-extender.php
@@ -4,13 +4,13 @@
 Plugin Name: Yikes, Inc Easy Mailchimp Extender
 Plugin URI: http://www.yikesinc.com
 Description: Mailchimp API integration in the form of a shortcode or php snippet
-Version: 2.0.1
+Version: 2.0.2
 Author: Yikes, Inc
 Author URI: http://www.yikesinc.com
 License: GPL2
 
 #_________________________________________________ LICENSE
-Copyright 2010 Sean Kennedy (email : sean@yikesinc.com)
+Copyright 2012 Yikes, Inc (email : tech@yikesinc.com)
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License, version 2, as 
@@ -29,9 +29,9 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #_________________________________________________ CONSTANTS
 /** Configuration **/
 if(!defined('YKSEME_DEBUG'))									define('YKSEME_DEBUG',				         true);
-if(!defined('YKSEME_VERSION_CURRENT'))				define('YKSEME_VERSION_CURRENT',			'2.0.0');
+if(!defined('YKSEME_VERSION_CURRENT'))				define('YKSEME_VERSION_CURRENT',			'2.0.2');
 if(!defined('YKSEME_REQ_PHP'))								define('YKSEME_REQ_PHP',							'5.0');
-if(!defined('YKSEME_AUTHOR'))									define('YKSEME_AUTHOR',								'Sean Kennedy');
+if(!defined('YKSEME_AUTHOR'))									define('YKSEME_AUTHOR',								'Yikes, Inc');
 if(!defined('YKSEME_SITE'))										define('YKSEME_SITE',									site_url().'/');
 if(!defined('YKSEME_PREFIX'))									define('YKSEME_PREFIX',								'ykseme_');
 if(!defined('YKSEME_PATH'))										define('YKSEME_PATH',									ABSPATH.'wp-content/plugins/yikes-inc-easy-mailchimp-extender/');
