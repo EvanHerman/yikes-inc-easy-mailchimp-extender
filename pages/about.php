d113c77f7d2c0ec3c5b58cde6454af4e0dcab641
Tracy Levesque <tracy@yikesinc.com>
2013-10-18 14:41:48 -0400

    Revised the about.php page

--- pages/about.php
+++ pages/about.php
@@ -1,5 +1,5 @@
-<div class="wrap">
-	<div id="ykseme-icon" class="icon32"><br /></div>
+<div class="wrap yksme-page-about">
+	<div id="ykseme-icon" class="icon32"></div>
 	
 	<h2 id="ykseme-page-header">
 		About YIKES, Inc.
@@ -7,31 +7,41 @@
 
 	<div id="yksme-page-content">
 		<div id="yksme-yikes-logo-container">
-		<a href="http://www.yikesinc.com" title="YIKES, Inc." target="_blank"><img src="<?php echo YKSEME_URL; ?>/images/yikes_logo.png" alt="YIKES, Inc." id="yksme-yikes-logo" /></a>
+			<a href="http://www.yikesinc.com" title="YIKES, Inc." target="_blank"><img src="<?php echo YKSEME_URL; ?>/images/yikes_logo.png" alt="YIKES, Inc." id="yksme-yikes-logo" /></a>
 		
 			<div class="b-sbn">
-			<a href="http://www.bcorporation.net/yikes" target="_blank"><img src="<?php echo YKSEME_URL; ?>/images/bcorp.jpg" alt="Certified B Corporation"></a>
-			<a href="http://www.sbnphiladelphia.org/" target="_blank"><img src="<?php echo YKSEME_URL; ?>/images/sbn_logo.png" alt="YA proud sponsor of the Sustainable Business Network of Philadelphia"></a>
+				<a href="http://www.bcorporation.net/yikes" target="_blank"><img src="<?php echo YKSEME_URL; ?>/images/bcorp.jpg" alt="Certified B Corporation"></a>
+				<a href="http://www.sbnphiladelphia.org/" target="_blank"><img src="<?php echo YKSEME_URL; ?>/images/sbn_logo.png" alt="Proud sponsor of the Sustainable Business Network of Philadelphia"></a>
 			</div>
 		
-		<a href="http://facebook.com/yikesinc" target="_blank"><img src="<?php echo YKSEME_URL; ?>/images/facebook.png" style="border: 0px none;" alt="YIKES Philadelphia Web design and Development Facebook" height="24" width="24"></a> <a href="http://twitter.com/yikesinc" target="_blank"><img src="<?php echo YKSEME_URL; ?>/images/twitter.png" style="border: 0px none;" alt="YIKES Philadelphia Web design and Development Twitter" height="24" width="24"></a> <script gapi_processed="true" type="text/javascript" src="http://apis.google.com/js/plusone.js"></script>
-		<div style="height: 24px; width: 106px; display: inline-block; text-indent: 0pt; margin: 0pt; padding: 0pt; background: none repeat scroll 0% 0% transparent; border-style: none; float: none; line-height: normal; font-size: 1px; vertical-align: baseline;" id="___plusone_0"><iframe allowtransparency="true" hspace="0" id="I1_1333575714870" marginheight="0" marginwidth="0" name="I1_1333575714870" src="https://plusone.google.com/_/+1/fastbutton?url=http%3A%2F%2Fwww.yikesinc.com%2F&amp;size=standard&amp;count=true&amp;hl=en-US&amp;jsh=m%3B%2F_%2Fapps-static%2F_%2Fjs%2Fgapi%2F__features__%2Frt%3Dj%2Fver%3DkJTTGFio1Bk.en.%2Fsv%3D1%2Fam%3D%21Ze6NnRS0VYCICGRMrA%2Fd%3D1%2Frs%3DAItRSTO0675vsQCIIVLyCqE1jWrDTqqpjw#id=I1_1333575714870&amp;parent=http%3A%2F%2Fwww.yikesinc.com&amp;rpctoken=371844436&amp;_methods=onPlusOne%2C_ready%2C_close%2C_open%2C_resizeMe%2C_renderstart" style="position: static; left: 0pt; top: 0pt; width: 106px; margin: 0px; border-style: none; height: 24px; visibility: visible;" tabindex="0" vspace="0" title="+1" frameborder="0" scrolling="no" width="100%"></iframe></div><br>
-		 <a href="http://yikesinc.tumblr.com/" target="_blank"><img src="<?php echo YKSEME_URL; ?>/images/tumblr.png" style="border: 0px none;" alt="YIKES Philadelphia Web design and Development Tumblr" height="24" width="24"></a> 
-		<a href="http://www.linkedin.com/companies/yikes-inc" title="Linkedin" target="_blank"><img src="<?php echo YKSEME_URL; ?>/images/linkedin.png" alt="YIKES Philadelphia Web design and Development Linkedin" border="0" height="24" width="24"></a>
-		<a href="http://www.flickr.com/photos/yikesinc/" target="_blank"><img src="<?php echo YKSEME_URL; ?>/images/flickr.png" style="border: 0px none;" alt="YIKES Philadelphia Web design and Development Flickr" height="24" width="24"></a>
-		<a href="http://www.youtube.com/yikesinc/" target="_blank"><img src="<?php echo YKSEME_URL; ?>/images/youtube.png" style="border: 0px none;" alt="YIKES Philadelphia Web design and Development YouTube" height="24" width="24"></a>
-		<a href="http://pinterest.com/yikesinc/" target="_blank"><img src="<?php echo YKSEME_URL; ?>/images/PinterestIcon.png" style="border: 0px none;" alt="YIKES Philadelphia Web design and Development Pinterest" height="24" width="24"></a> 
+			<a href="http://facebook.com/yikesinc" target="_blank"><img src="<?php echo YKSEME_URL; ?>/images/facebook.png" style="border: 0px none;" alt="YIKES Philadelphia Web design and Development Facebook" height="24" width="24"></a> 
+			<a href="http://twitter.com/yikesinc" target="_blank"><img src="<?php echo YKSEME_URL; ?>/images/twitter.png" style="border: 0px none;" alt="YIKES Philadelphia Web design and Development Twitter" height="24" width="24"></a> 
+			<a href="http://www.linkedin.com/companies/yikes-inc" title="Linkedin" target="_blank"><img src="<?php echo YKSEME_URL; ?>/images/linkedin.png" alt="YIKES Philadelphia Web design and Development Linkedin" border="0" height="24" width="24"></a>
+
+			<script gapi_processed="true" type="text/javascript" src="http://apis.google.com/js/plusone.js"></script>
+			<div style="height: 24px; width: 106px; display: inline-block; text-indent: 0pt; margin: 0pt; padding: 0pt; background: none repeat scroll 0% 0% transparent; border-style: none; float: none; line-height: normal; font-size: 1px; vertical-align: baseline;" id="___plusone_0">
+				<iframe allowtransparency="true" hspace="0" id="I1_1333575714870" marginheight="0" marginwidth="0" name="I1_1333575714870" src="https://plusone.google.com/_/+1/fastbutton?url=http%3A%2F%2Fwww.yikesinc.com%2F&amp;size=standard&amp;count=true&amp;hl=en-US&amp;jsh=m%3B%2F_%2Fapps-static%2F_%2Fjs%2Fgapi%2F__features__%2Frt%3Dj%2Fver%3DkJTTGFio1Bk.en.%2Fsv%3D1%2Fam%3D%21Ze6NnRS0VYCICGRMrA%2Fd%3D1%2Frs%3DAItRSTO0675vsQCIIVLyCqE1jWrDTqqpjw#id=I1_1333575714870&amp;parent=http%3A%2F%2Fwww.yikesinc.com&amp;rpctoken=371844436&amp;_methods=onPlusOne%2C_ready%2C_close%2C_open%2C_resizeMe%2C_renderstart" style="position: static; left: 0pt; top: 0pt; width: 106px; margin: 0px; border-style: none; height: 24px; visibility: visible;" tabindex="0" vspace="0" title="+1" frameborder="0" scrolling="no" width="100%"></iframe>
+			</div>
 		</div>
 		
 		<h3>YIKES, Inc. Web Design and Development</h3>
 		
-		<h4>Smart, effective, beautifully designed web solutions for the Greater Philadelphia region and beyond. </h4>
+		<h4>Smart, effective, beautifully designed web solutions</h4>
+
+		<p>
+			YIKES is located in the Fishtown neighborhood of Philadelphia where we build custom WordPress themes and plugins every day.
+		</p>
+
+		<p> At YIKES we love WordPress! Co-Owner, Tracy Levesque has presented at WordCamps <a href="http://wordpress.tv/2013/04/12/tracy-levesque-custom-post-types-for-right-brained-folks/" target="_blank">Philly</a>, NYC, Montreal and <a href="http://wordpress.tv/2013/07/31/tracy-levesque-what-you-dont-know-you-can-do-wordpress-development-for-absolutely-everyone/" target="_blank">San Francisco. She also teaches WordPress classes for <a href="http://www.meetup.com/Girl-Develop-It-Philadelphia/events/134330962/" target="_blank">Girl Develop It Philadelphia</a>.
+		</p>
+
+		<p>
+			In addition to making great WordPress sites we are also committed to the sustainable business movement. We observe the triple bottom line: people, planet, profit and work to help build a more socially, environmentally and financially sustainable local economy. We even have a <a href="http://www.yikesinc.com/company/our-office/" target="_blank">LEED Platinum office</a>.
+		</p>
 		
-		<p>Since 1996 YIKES, Inc. has built creative web design and development solutions for businesses and non-profit organizations. We specialize in creating custom WordPress themes and plugins.</p>
-		
-		<p>In addition to making great WordPress sites YIKES is also committed to being a socially responsible business observing the triple bottom line: people, planet, profit. We work to help build a more socially, environmentally and financially sustainable local economy.</p>
-		
-		<p>Learn more about us at <a href="http://www.yikesinc.com" title="YIKES, Inc." target="_blank">www.yikesinc.com</a></p>
+		<p>
+			If you're interested in hiring us for WordPress development or want to learn more about us at please visit <a href="http://www.yikesinc.com" title="YIKES, Inc." target="_blank">yikesinc.com</a>
+		</p>
 	</div>
 	
 </div>
