2ef03719192f1e37e1d340cafa6621e0e597be75
Evan Herman <evan@yikesinc.com>
2015-05-22 16:58:11 -0400

    start transitioning from 'pro' to 'add-ons'

--- plugin_core/admin/partials/menu/add-ons.php
+++ plugin_core/admin/partials/menu/add-ons.php
@@ -9,7 +9,7 @@
 	<!-- Freddie Logo -->
 	<img src="<?php echo YIKES_MC_URL . 'includes/images/MailChimp_Assets/Freddie_60px.png'; ?>" alt="Freddie - MailChimp Mascot" style="float:left;margin-right:10px;" />
 		
-	<h2>Easy MailChimp by Yikes Inc. | <?php echo __( 'Add-Ons' , $this->text_domain ); ?> &nbsp; <a href="#" onclick="return false;" target="_blank" class="button-primary"><?php _e( 'View All Add-Ons' , $this->text_domain ); ?></a></h2>				
+	<h2>Easy MailChimp by Yikes Inc. | <?php echo __( 'Add-Ons' , $this->text_domain ); ?> &nbsp; <a href="#" onclick="return false;" disabled target="_blank" class="button-primary" style="position:absolute;" title="<?php _e( 'Coming Soon...' , $this->text_domain ); ?>"><?php _e( 'View All Add-Ons' , $this->text_domain ); ?> <span class="dashicons dashicons-external" style="font-size:14px;line-height:1.8;"></span></a></a></h2>				
 		
 	<!-- Addons Page Description -->
 	<p class="yikes-easy-mc-about-text about-text" style="margin-top:0;"><?php _e( "Below you'll find a complete list of available add-ons for Easy MailChimp by Yikes Inc. Each add-on extends the base functionality of the plugin in some shape or form." , $this->text_domain ); ?></p>
@@ -17,51 +17,34 @@
 	<!-- Add-On Container -->
 	<section id="add-ons" style="margin: 3em 0;">
 	
-		<div class="post-354 download type-download status-publish has-post-thumbnail hentry download_category-payment-gateways download_category-tools edd-download edd-download-cat-payment-gateways edd-download-cat-tools">
+		<p><?php _e( 'Add-ons coming soon...' , $this->text_domain ); ?></p>
+	
+		<!--
+			<div class="post-354 download type-download status-publish has-post-thumbnail hentry download_category-payment-gateways download_category-tools edd-download edd-download-cat-payment-gateways edd-download-cat-tools">
 
-			<div class="featured-img">
-				<a href="http://www.yikesinc.com/" title="<?php _e( 'Add-on Title' , $this->text_domain ); ?>" target="_blank">
-					<!-- <img src="#" width="280" height="164" alt="<?php _e( 'Add-on Title' , $this->text_domain ); ?>"> -->
-					<h4 style="display:block;text-align:center;">Add-on Image</h4>
-				</a>		
+				<div class="featured-img">
+					<a href="http://www.yikesinc.com/" title="<?php _e( 'Add-on Title' , $this->text_domain ); ?>" target="_blank">
+						<img src="#" width="280" height="164" alt="<?php _e( 'Add-on Title' , $this->text_domain ); ?>">
+						<h4 style="display:block;text-align:center;">Add-on Image</h4>
+					</a>		
+				</div>
+
+				<div class="addon-content">
+					<h3 class="addon-heading">
+						<a href="http://www.yikesinc.com/" title="<?php _e( 'Add-on Title' , $this->text_domain ); ?>" target="_blank">Campaign Statistics</a>
+					</h3>
+					<p>Use our advanced form customizer to style your forms to better fit the look and feel of your site (without touching any code!).</p>
+				</div>
+
+				<div class="addon-footer-wrap give-clearfix">
+					<a href="http://www.yikesinc.com/" title="<?php _e( 'Add-on Title' , $this->text_domain ); ?>" class="button-secondary" target="_blank">
+						<?php _e( 'Learn More' , $this->text_domain ); ?>	
+					<span class="dashicons dashicons-external"></span></a>
+				</div>
+
 			</div>
-
-			<div class="addon-content">
-				<h3 class="addon-heading">
-					<a href="http://www.yikesinc.com/" title="<?php _e( 'Add-on Title' , $this->text_domain ); ?>" target="_blank">Campaign Statistics</a>
-				</h3>
-				<p>Use our advanced form customizer to style your forms to better fit the look and feel of your site (without touching any code!).</p>
-			</div>
-
-			<div class="addon-footer-wrap give-clearfix">
-				<a href="http://www.yikesinc.com/" title="<?php _e( 'Add-on Title' , $this->text_domain ); ?>" class="button-secondary" target="_blank">
-					<?php _e( 'Learn More' , $this->text_domain ); ?>	
-				<span class="dashicons dashicons-external"></span></a>
-			</div>
-
-		</div>
-
-		<div class="post-245 download type-download status-publish has-post-thumbnail hentry download_category-payment-gateways edd-download edd-download-cat-payment-gateways">
-
-			<div class="featured-img">
-				<a href="http://www.yikesinc.com/" title="<?php _e( 'Add-on Title' , $this->text_domain ); ?>" target="_blank">
-					<h4 style="display:block;text-align:center;">Add-on Image</h4>
-				</a>		
-			</div>
-
-			<div class="addon-content">
-				<h3 class="addon-heading">
-					<a href="http://www.yikesinc.com/" title="<?php _e( 'Add-on Title' , $this->text_domain ); ?>" target="_blank">Form Customizer</a>
-				</h3>
-				<p>Use our advanced form customizer to style your forms to better fit the look and feel of your site (without touching any code!).</p>
-			</div>
-
-			<div class="addon-footer-wrap give-clearfix">
-				<a href="http://www.yikesinc.com/" title="<?php _e( 'Add-on Title' , $this->text_domain ); ?>" class="button-secondary" target="_blank">
-					<?php _e( 'Learn More' , $this->text_domain ); ?>	
-				<span class="dashicons dashicons-external"></span></a>
-			</div>
-
-		</div>
-
+		-->
+		
 	</section>
+	
+</div>
