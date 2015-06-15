74fc4079929f8a24a3b71223e4ea3a38416f1963
Evan Herman <evan@yikesinc.com>
2015-06-15 15:27:31 -0400

    Fixed customizer widget title icon - [#303](https://github.com/yikesinc/yikes-inc-easy-mailchimp-extender/issues/303) Removed ALL inline styles from the dashboard and front-end (some fields require dynamic display:none; but very fiew) - [#281](https://github.com/yikesinc/yikes-inc-easy-mailchimp-extender/issues/281) Repaired broken internationalized text strings - [#297](https://github.com/yikesinc/yikes-inc-easy-mailchimp-extender/issues/297) Fixed undefined description variable in the widget if not description was set for the form Repair undefined variable when form was not previously set, but the global $_POST variable was set Fixed our notice not passing the plugin name properly Adjust button styles in our custom table

--- plugin_core/admin/partials/menu/add-ons.php
+++ plugin_core/admin/partials/menu/add-ons.php
@@ -9,13 +9,13 @@
 	<!-- Freddie Logo -->
 	<img src="<?php echo YIKES_MC_URL . 'includes/images/MailChimp_Assets/Freddie_60px.png'; ?>" alt="<?php __( 'Freddie - MailChimp Mascot' , 'yikes-inc-easy-mailchimp-extender' ); ?>" class="yikes-mc-freddie-logo" />
 		
-	<h2>Easy MailChimp by Yikes Inc. | <?php echo __( 'Add-Ons' , 'yikes-inc-easy-mailchimp-extender' ); ?> &nbsp; <a href="#" onclick="return false;" disabled target="_blank" class="button-primary" style="position:absolute;" title="<?php _e( 'Coming Soon...' , 'yikes-inc-easy-mailchimp-extender' ); ?>"><?php _e( 'View All Add-Ons' , 'yikes-inc-easy-mailchimp-extender' ); ?> <span class="dashicons dashicons-external" style="font-size:14px;line-height:1.8;"></span></a></a></h2>				
+	<h2>Easy MailChimp by Yikes Inc. | <?php echo __( 'Add-Ons' , 'yikes-inc-easy-mailchimp-extender' ); ?> &nbsp; <a href="#" onclick="return false;" disabled target="_blank" class="button-primary coming-soon-button" title="<?php _e( 'Coming Soon...' , 'yikes-inc-easy-mailchimp-extender' ); ?>"><?php _e( 'View All Add-Ons' , 'yikes-inc-easy-mailchimp-extender' ); ?> <span class="dashicons dashicons-external"></span></a></h2>				
 		
 	<!-- Addons Page Description -->
-	<p class="yikes-easy-mc-about-text about-text" style="margin-top:0;"><?php _e( "Below you'll find a complete list of available add-ons for Easy MailChimp by Yikes Inc. Each add-on extends the base functionality of the plugin in some shape or form." , 'yikes-inc-easy-mailchimp-extender' ); ?></p>
+	<p class="yikes-easy-mc-about-text about-text"><?php _e( "Below you'll find a complete list of available add-ons for Easy MailChimp by Yikes Inc. Each add-on extends the base functionality of the plugin in some shape or form." , 'yikes-inc-easy-mailchimp-extender' ); ?></p>
 	
 	<!-- Add-On Container -->
-	<section id="add-ons" style="margin: 3em 0;">
+	<section id="add-ons">
 	
 		<p><?php _e( 'Add-ons coming soon...' , 'yikes-inc-easy-mailchimp-extender' ); ?></p>
 	
@@ -25,7 +25,7 @@
 				<div class="featured-img">
 					<a href="http://www.yikesinc.com/" title="<?php _e( 'Add-on Title' , 'yikes-inc-easy-mailchimp-extender' ); ?>" target="_blank">
 						<img src="#" width="280" height="164" alt="<?php _e( 'Add-on Title' , 'yikes-inc-easy-mailchimp-extender' ); ?>">
-						<h4 style="display:block;text-align:center;">Add-on Image</h4>
+						<h4 class="addon-image">Add-on Image</h4>
 					</a>		
 				</div>
 
