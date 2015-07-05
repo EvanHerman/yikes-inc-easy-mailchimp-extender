9528b37218f37d97560fc6dc239029c6ea2d4371
Evan Herman <evan@yikesinc.com>
2015-07-05 11:06:47 -0400

    add wysiwyg field handler

---
+++ plugin_core/admin/partials/helpers/fields/yikes-mailchimp-wysiwyg-field.php
@@ -0,0 +1,15 @@
+<?php
+	/*
+	* Upload WYSIWYG Input Field
+	* @since 6.0
+	*/
+	$field_data = json_decode( $form_data['custom_fields'] , true ); 
+	$content = isset( $field_data[$field['id']] ) ? $field_data[$field['id']] : '';
+	$wysiwyg_id = 'custom-field['.$field['id'].']';
+?>
+
+<div class="yikes-mailchimp-wysiwyg-field">
+    <label for="image_url" class="widefat"><strong><?php echo $field['label']; ?></strong></label>
+    <?php wp_editor( $content, $field['id'], array( 'textarea_name' => $wysiwyg_id ) ); ?>
+    <p class="description"><?php echo $field['description']; ?></p>
+</div>
