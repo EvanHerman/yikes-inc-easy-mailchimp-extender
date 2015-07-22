c403f26c22aa8f5b6224c7432680b620de53881a
Evan Herman <evan@yikesinc.com>
2015-07-22 02:26:40 -0400

    Finished add-ons page with MailChimp products RSS feed integration

--- plugin_core/admin/partials/menu/add-ons.php
+++ plugin_core/admin/partials/menu/add-ons.php
@@ -3,48 +3,93 @@
 	*	Main page to list our current add-ons
 	*	@since 6.0.0
 	*/
+	// RSS Feed https://yikesplugins.com/feed/?post_type=download&download_category=MailChimp&download_tag=add-on
 ?>
 <div class="wrap">
 	
 	<!-- Freddie Logo -->
 	<img src="<?php echo YIKES_MC_URL . 'includes/images/MailChimp_Assets/Freddie_60px.png'; ?>" alt="<?php __( 'Freddie - MailChimp Mascot' , 'yikes-inc-easy-mailchimp-extender' ); ?>" class="yikes-mc-freddie-logo" />
 		
-	<h2>Easy MailChimp by YIKES Inc. | <?php echo __( 'Add-Ons' , 'yikes-inc-easy-mailchimp-extender' ); ?> &nbsp; <a href="#" onclick="return false;" disabled target="_blank" class="button-primary coming-soon-button" title="<?php _e( 'Coming Soon...' , 'yikes-inc-easy-mailchimp-extender' ); ?>"><?php _e( 'View All Add-Ons' , 'yikes-inc-easy-mailchimp-extender' ); ?> <span class="dashicons dashicons-external"></span></a></h2>				
+	<h2>Easy MailChimp by YIKES Inc. | <?php echo __( 'Add-Ons' , 'yikes-inc-easy-mailchimp-extender' ); ?> &nbsp; <a href="https://yikesplugins.com/plugins/?plugins=MailChimp" target="_blank" class="button-primary coming-soon-button" title="<?php _e( 'View All Add-Ons' , 'yikes-inc-easy-mailchimp-extender' ); ?>"><?php _e( 'View All Add-Ons' , 'yikes-inc-easy-mailchimp-extender' ); ?> <span class="dashicons dashicons-external"></span></a></h2>				
 		
 	<!-- Addons Page Description -->
 	<p class="yikes-easy-mc-about-text about-text"><?php _e( "Below you'll find a complete list of available add-ons for Easy MailChimp by YIKES Inc. Each add-on extends the base functionality of the plugin in some shape or form." , 'yikes-inc-easy-mailchimp-extender' ); ?></p>
 	
 	<!-- Add-On Container -->
 	<section id="add-ons">
-	
-		<p><?php _e( 'Add-ons coming soon...' , 'yikes-inc-easy-mailchimp-extender' ); ?></p>
-	
-		<!--
-			<div class="post-354 download type-download status-publish has-post-thumbnail hentry download_category-payment-gateways download_category-tools edd-download edd-download-cat-payment-gateways edd-download-cat-tools">
 
-				<div class="featured-img">
-					<a href="http://www.yikesinc.com/" title="<?php _e( 'Add-on Title' , 'yikes-inc-easy-mailchimp-extender' ); ?>" target="_blank">
-						<img src="#" width="280" height="164" alt="<?php _e( 'Add-on Title' , 'yikes-inc-easy-mailchimp-extender' ); ?>">
-						<h4 class="addon-image">Add-on Image</h4>
-					</a>		
+		<?php 
+			/*
+			*	SimplePie strips out all query strings
+			* 	we had to implement a workaround
+			*	https://github.com/simplepie/simplepie/issues/317
+			*/
+			// Get RSS Feed(s)
+			require_once( ABSPATH . WPINC . '/class-feed.php' );
+			/* Create the SimplePie object */
+			$feed = new SimplePie(); 
+			$feed_url = esc_url( 'https://yikesplugins.com/feed/?post_type=download&download_category=MailChimp&download_tag=add-on' );
+			/* Set the URL of the feed you're retrieving */
+			$feed->set_feed_url( $feed_url );
+			/* Tell SimplePie to cache the feed using WordPress' cache class */
+			$feed->set_cache_class( 'WP_Feed_Cache' );
+			/* Tell SimplePie to use the WordPress class for retrieving feed files */
+			$feed->set_file_class( 'WP_SimplePie_File' );
+			$feed->enable_cache( false ); // temporary
+			/* Tell SimplePie how long to cache the feed data in the WordPress database */
+			$feed->set_cache_duration( apply_filters( 'wp_feed_cache_transient_lifetime', 43200, $feed_url ) );
+			/* Run any other functions or filters that WordPress normally runs on feeds */
+			do_action_ref_array( 'wp_feed_options', array( $feed, $feed_url ) ); 
+			/* Initiate the SimplePie instance */
+			$feed->init(); 
+			/* Tell SimplePie to send the feed MIME headers */
+			$feed->handle_content_type(); 
+			if ( $feed->error() ) {
+				return $feed = new WP_Error( 'simplepie-error', $feed->error() );
+			}
+			// print_r( $feed->get_items() );
+			foreach( $feed->get_items() as $add_on ) {
+				// store the description
+				$description_pre_split = $add_on->get_description();
+				$permalink = esc_url( $add_on->get_permalink() );
+				$add_on_title = $add_on->get_title();
+				// extract the image
+				preg_match( '/<img[^>]+\>/i', $description_pre_split, $split_string );
+				// store the content sans image tag
+				$description = preg_replace( "/<img[^>]+\>/i", "", $add_on->get_description() ); 
+				// store the image
+				$image = ( count( $split_string ) > 0 ) ? $split_string[0] : '';
+				// if {hidden} is found, exclude it
+				if( strpos( $description_pre_split, '{hidden}' ) ) {
+					continue;
+				}
+				?>
+				<div class="type-download">
+
+					<div class="featured-img">
+						<a href="<?php echo $permalink; ?>" title="<?php _e( 'Add-on Title' , 'yikes-inc-easy-mailchimp-extender' ); ?>" target="_blank">
+							<?php echo $image; ?>
+						</a>		
+					</div>
+
+					<div class="addon-content">
+						<h3 class="addon-heading">
+							<a href="<?php echo $permalink; ?>" title="<?php echo $add_on_title; ?>" target="_blank"><?php echo $add_on_title; ?></a>
+						</h3>
+						<p><?php echo $description; ?></p>
+					</div>
+
+					<div class="addon-footer-wrap give-clearfix">
+						<a href="<?php echo $permalink; ?>" title="<?php echo $add_on_title; ?>" class="button-secondary" target="_blank">
+							<?php _e( 'View Add-on' , 'yikes-inc-easy-mailchimp-extender' ); ?>	
+						<span class="dashicons dashicons-external"></span></a>
+					</div>
+
 				</div>
+				<?php
+			}
+		?>
 
-				<div class="addon-content">
-					<h3 class="addon-heading">
-						<a href="http://www.yikesinc.com/" title="<?php _e( 'Add-on Title' , 'yikes-inc-easy-mailchimp-extender' ); ?>" target="_blank">Campaign Statistics</a>
-					</h3>
-					<p>Use our advanced form customizer to style your forms to better fit the look and feel of your site (without touching any code!).</p>
-				</div>
-
-				<div class="addon-footer-wrap give-clearfix">
-					<a href="http://www.yikesinc.com/" title="<?php _e( 'Add-on Title' , 'yikes-inc-easy-mailchimp-extender' ); ?>" class="button-secondary" target="_blank">
-						<?php _e( 'Learn More' , 'yikes-inc-easy-mailchimp-extender' ); ?>	
-					<span class="dashicons dashicons-external"></span></a>
-				</div>
-
-			</div>
-		-->
-		
 	</section>
 	
 </div>
