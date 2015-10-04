fc483aa29f1f303c24a9325c8d10b80fa8c5bfd2
Evan Herman <evan@yikesinc.com>
2015-10-04 12:25:50 -0400

    Fix typo on migration page

--- plugin_core/admin/partials/dashboard-widgets/templates/stats-list-template.php
+++ plugin_core/admin/partials/dashboard-widgets/templates/stats-list-template.php
@@ -1,1 +1,39 @@
-<?php/* The template file for displaying our stats in the Admin dashboard widget */?><section id="yikes-easy-mc-widget-stat-holder">	<h3><?php echo $list_data['data'][0]['name']; ?> <small><a href="<?php echo esc_url_raw( admin_url( 'admin.php?page=yikes-mailchimp-view-list&list-id=' . $list_data['data'][0]['id'] . '' ) ); ?>" title="<?php _e( 'view List' , 'yikes-inc-easy-mailchimp-extender' ); ?>"><?php _e( 'view list' , 'yikes-inc-easy-mailchimp-extender' ); ?></a></small></h3>		<table class="yikes-easy-mc-stats-table">		<thead class="yikes-easy-mc-hidden">			<tr>				<th><?php _e( 'Subscribers' , 'yikes-inc-easy-mailchimp-extender' ); ?></th>				<th><?php _e( 'Unsubscribed' , 'yikes-inc-easy-mailchimp-extender' ); ?></th>				<th><?php _e( 'New Since Send' , 'yikes-inc-easy-mailchimp-extender' ); ?></th>					<th><?php _e( 'Avg. Sub. Rate' , 'yikes-inc-easy-mailchimp-extender' ); ?></th>			</tr>		</thead>		<tbody>			<tr class="yikes-easy-mc-table-stats-tr yikes-easy-mc-table-stats-tr-first">				<td title="<?php _e( 'Number of active subscribers.' , 'yikes-inc-easy-mailchimp-extender' ); ?>">					<p class="yikes-easy-mc-dashboard-stat"><?php echo $list_data['data'][0]['stats']['member_count']; ?></p>						<p class="yikes-easy-mc-stat-list-label"><?php _e( 'subscribers' , 'yikes-inc-easy-mailchimp-extender' ); ?></p>				</td>				<td title="<?php _e( 'Number of users who have unsusbscribed.' , 'yikes-inc-easy-mailchimp-extender' ); ?>">					<p class="yikes-easy-mc-dashboard-stat"><?php echo $list_data['data'][0]['stats']['unsubscribe_count']; ?></p>						<p class="yikes-easy-mc-stat-list-label"><?php _e( 'unsubscribed' , 'yikes-inc-easy-mailchimp-extender' ); ?></p>				</td>			</tr>			<tr class="yikes-easy-mc-table-stats-tr  yikes-easy-mc-table-stats-tr-second">				<td title="<?php _e( 'Number of new subscribers since the last campaign was sent.' , 'yikes-inc-easy-mailchimp-extender' ); ?>">					<p class="yikes-easy-mc-dashboard-stat"><?php echo $list_data['data'][0]['stats']['member_count_since_send']; ?></p>						<p class="yikes-easy-mc-stat-list-label"><?php _e( 'new since send' , 'yikes-inc-easy-mailchimp-extender' ); ?></p>				</td>				<td title="<?php _e( 'Average number of subscribers per month.' , 'yikes-inc-easy-mailchimp-extender' ); ?>">					<p class="yikes-easy-mc-dashboard-stat"><?php echo $list_data['data'][0]['stats']['avg_sub_rate']; ?></p>						<p class="yikes-easy-mc-stat-list-label"><?php _e( 'avg. sub. rate' , 'yikes-inc-easy-mailchimp-extender' ); ?></p>				</td>			</tr>		</tbody>	</table></section>
+<?php
+/* The template file for displaying our stats in the Admin dashboard widget */
+?>
+<section id="yikes-easy-mc-widget-stat-holder">
+	<h3><?php echo $list_data['data'][0]['name']; ?> <small><a href="<?php echo esc_url_raw( admin_url( 'admin.php?page=yikes-mailchimp-view-list&list-id=' . $list_data['data'][0]['id'] . '' ) ); ?>" title="<?php _e( 'view List' , 'yikes-inc-easy-mailchimp-extender' ); ?>"><?php _e( 'view list' , 'yikes-inc-easy-mailchimp-extender' ); ?></a></small></h3>
+	
+	<table class="yikes-easy-mc-stats-table">
+		<thead class="yikes-easy-mc-hidden">
+			<tr>
+				<th><?php _e( 'Subscribers' , 'yikes-inc-easy-mailchimp-extender' ); ?></th>
+				<th><?php _e( 'Unsubscribed' , 'yikes-inc-easy-mailchimp-extender' ); ?></th>
+				<th><?php _e( 'New Since Send' , 'yikes-inc-easy-mailchimp-extender' ); ?></th>	
+				<th><?php _e( 'Avg. Sub. Rate' , 'yikes-inc-easy-mailchimp-extender' ); ?></th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr class="yikes-easy-mc-table-stats-tr yikes-easy-mc-table-stats-tr-first">
+				<td title="<?php _e( 'Number of active subscribers.' , 'yikes-inc-easy-mailchimp-extender' ); ?>">
+					<p class="yikes-easy-mc-dashboard-stat"><?php echo $list_data['data'][0]['stats']['member_count']; ?></p>
+						<p class="yikes-easy-mc-stat-list-label"><?php _e( 'subscribers' , 'yikes-inc-easy-mailchimp-extender' ); ?></p>
+				</td>
+				<td title="<?php _e( 'Number of users who have unsusbscribed.' , 'yikes-inc-easy-mailchimp-extender' ); ?>">
+					<p class="yikes-easy-mc-dashboard-stat"><?php echo $list_data['data'][0]['stats']['unsubscribe_count']; ?></p>
+						<p class="yikes-easy-mc-stat-list-label"><?php _e( 'unsubscribed' , 'yikes-inc-easy-mailchimp-extender' ); ?></p>
+				</td>
+			</tr>
+			<tr class="yikes-easy-mc-table-stats-tr  yikes-easy-mc-table-stats-tr-second">
+				<td title="<?php _e( 'Number of new subscribers since the last campaign was sent.' , 'yikes-inc-easy-mailchimp-extender' ); ?>">
+					<p class="yikes-easy-mc-dashboard-stat"><?php echo $list_data['data'][0]['stats']['member_count_since_send']; ?></p>
+						<p class="yikes-easy-mc-stat-list-label"><?php _e( 'new since send' , 'yikes-inc-easy-mailchimp-extender' ); ?></p>
+				</td>
+				<td title="<?php _e( 'Average number of subscribers per month.' , 'yikes-inc-easy-mailchimp-extender' ); ?>">
+					<p class="yikes-easy-mc-dashboard-stat"><?php echo $list_data['data'][0]['stats']['avg_sub_rate']; ?></p>
+						<p class="yikes-easy-mc-stat-list-label"><?php _e( 'avg. sub. rate' , 'yikes-inc-easy-mailchimp-extender' ); ?></p>
+				</td>
+			</tr>
+		</tbody>
+	</table>
+</section>
--- plugin_core/admin/partials/menu/options-sections/api-cache-settings.php
+++ plugin_core/admin/partials/menu/options-sections/api-cache-settings.php
@@ -1,1 +1,21 @@
-<!--	MailChimp API Clear Stored Cache Template	-	Clear MailChimp transient data such as forms, form fields, list data, subscribers etc.		*	transient cache is stored for 60 minutes.--><h3><span><?php _e( 'API Cache Settings' , 'yikes-inc-easy-mailchimp-extender' ); ?></span></h3><div class="inside">										<!-- Settings Form -->	<form action="<?php echo esc_url_raw( add_query_arg( array( 'action' => 'yikes-easy-mc-clear-transient-data' , 'nonce' => wp_create_nonce( 'clear-mc-transient-data' ) ) ) ); ?>" method="post">																		<p><?php _e( "Delete all MailChimp data stored in your sites cache. Most data is stored in the cache for 1 hour." , 'yikes-inc-easy-mailchimp-extender' ); ?></p>			<!-- check if any of our transients contain data -->										<?php if ( false === get_transient( 'yikes-easy-mailchimp-list-data' ) && false === get_transient( 'yikes-easy-mailchimp-profile-data' ) && false === get_transient( 'yikes-easy-mailchimp-account-data' ) ) { ?>				<p><a href="#" class="button-secondary" disabled="disabled" title="<?php _e( 'No MailChimp data found in temporary cache storage.' , 'yikes-inc-easy-mailchimp-extender' ); ?>"><?php _e( 'Clear MailChimp API Cache' , 'yikes-inc-easy-mailchimp-extender' ); ?></a></p>			<?php } else { ?>				<p><input type="submit" class="button-primary" value="<?php _e( 'Clear MailChimp API Cache' , 'yikes-inc-easy-mailchimp-extender' ); ?>" /></p>			<?php } ?>										</form></div> <!-- .inside -->
+<!--
+	MailChimp API Clear Stored Cache Template
+	-	Clear MailChimp transient data such as forms, form fields, list data, subscribers etc.
+		*	transient cache is stored for 60 minutes.
+-->
+<h3><span><?php _e( 'API Cache Settings' , 'yikes-inc-easy-mailchimp-extender' ); ?></span></h3>
+<div class="inside">
+									
+	<!-- Settings Form -->
+	<form action="<?php echo esc_url_raw( add_query_arg( array( 'action' => 'yikes-easy-mc-clear-transient-data' , 'nonce' => wp_create_nonce( 'clear-mc-transient-data' ) ) ) ); ?>" method="post">							
+									
+		<p><?php _e( "Delete all MailChimp data stored in your sites cache. Most data is stored in the cache for 1 hour." , 'yikes-inc-easy-mailchimp-extender' ); ?></p>
+			<!-- check if any of our transients contain data -->							
+			<?php if ( false === get_transient( 'yikes-easy-mailchimp-list-data' ) && false === get_transient( 'yikes-easy-mailchimp-profile-data' ) && false === get_transient( 'yikes-easy-mailchimp-account-data' ) ) { ?>
+				<p><a href="#" class="button-secondary" disabled="disabled" title="<?php _e( 'No MailChimp data found in temporary cache storage.' , 'yikes-inc-easy-mailchimp-extender' ); ?>"><?php _e( 'Clear MailChimp API Cache' , 'yikes-inc-easy-mailchimp-extender' ); ?></a></p>
+			<?php } else { ?>
+				<p><input type="submit" class="button-primary" value="<?php _e( 'Clear MailChimp API Cache' , 'yikes-inc-easy-mailchimp-extender' ); ?>" /></p>
+			<?php } ?>
+									
+	</form>
+</div> <!-- .inside -->
