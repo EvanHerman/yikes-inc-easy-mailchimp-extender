a44e862b5b4e312a12808222da793d64dbeae141
Evan Herman <evan@yikesinc.com>
2015-10-29 20:20:08 -0400

    release final v6.0.3

--- plugin_core/includes/third-party-integrations/third-party-init.php
+++ plugin_core/includes/third-party-integrations/third-party-init.php
@@ -14,12 +14,13 @@ class YIKES_MailChimp_ThirdParty_Integrations {
 	*
 	*/
 	public function load_third_party_integrations() {
+		// required..*
+		include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 		/* Visual Composer */
 		if( is_plugin_active( 'js_composer/js_composer.php' ) ) {
 			include_once( YIKES_MC_PATH . 'includes/third-party-integrations/visual-composer/visual-composer.php' );
 		}
 	}
-
 
 }
 
