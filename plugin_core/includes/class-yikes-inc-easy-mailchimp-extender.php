9850396e6e8b6c7a293363234377e9691b146150
Evan Herman <evan@yikesinc.com>
2015-12-28 16:13:35 -0500

    Removed extra closing php tag after 'Interest Group' dropdown field

--- plugin_core/includes/class-yikes-inc-easy-mailchimp-extender.php
+++ plugin_core/includes/class-yikes-inc-easy-mailchimp-extender.php
@@ -62,7 +62,7 @@ class Yikes_Inc_Easy_Mailchimp_Extender {
 	 */
 	public function __construct() {
 		$this->yikes_inc_easy_mailchimp_extender = 'yikes-inc-easy-mailchimp-extender';
-		$this->version = '6.0.3.5';
+		$this->version = '6.0.3.6';
 		$this->load_dependencies();
 		$this->set_locale();
 		$this->define_admin_hooks();
