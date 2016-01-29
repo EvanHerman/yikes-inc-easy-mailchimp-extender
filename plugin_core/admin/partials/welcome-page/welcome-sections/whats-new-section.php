25b2b99d0bd845c2ccadeb8a88f15f15529c7b7a
Evan Herman <evan@yikesinc.com>
2016-01-29 18:30:50 -0500

    delete old images update whats-new-section.php add glotpress-translation-percentage.png

--- plugin_core/admin/partials/welcome-page/welcome-sections/whats-new-section.php
+++ plugin_core/admin/partials/welcome-page/welcome-sections/whats-new-section.php
@@ -5,6 +5,7 @@
 		<h2><?php _e( 'All New Form Settings' , 'yikes-inc-easy-mailchimp-extender' ); ?></h2>
 		
 		<img src="<?php echo YIKES_MC_URL . 'includes/images/Welcome_Page/form-layout-option.jpg'; ?>" alt="<?php _e( 'New Form Settings', 'yikes-inc-easy-mailchimp-extender' ); ?>" class="yikes-easy-mc-feature-image yikes-easy-mc-whats-new-section-image">
+		
 		<img src="<?php echo YIKES_MC_URL . 'includes/images/Welcome_Page/inline-form-frontend.png'; ?>" style="margin-top:2em;padding-bottom: 2em;" alt="<?php _e( 'New Form Settings', 'yikes-inc-easy-mailchimp-extender' ); ?>" class="yikes-easy-mc-feature-image yikes-easy-mc-whats-new-section-image">
 		
 		<p><?php _e( "In this latest release we've added an additional layer of customization. You now have a whole new set of options to tweak on the edit form page - allowing an additional depth of customization.'", 'yikes-inc-easy-mailchimp-extender' ); ?></p>
@@ -37,26 +38,21 @@
 	
 	<div class="feature-section">
 		
-		<h2><?php _e( 'Knowledge Base' , 'yikes-inc-easy-mailchimp-extender' ); ?></h2>
-		
-		<p><?php _e( "Since the initial release of this Easy Forms for MailChimp by YIKES we've been tirelessly working on putting together a collection of help articles ranging from troubleshooting tips to customization options. We've also included a bunhc of code snippets from some of our most widely asked questions. Now you can access some of our most popular articles right from within the plugin.",  'yikes-inc-easy-mailchimp-extender' ); ?></p>
-		
+		<h2><?php _e( 'Knowledge Base & GlotPress Support' , 'yikes-inc-easy-mailchimp-extender' ); ?></h2>
+				
+		<img style="float:left;max-width:300px;margin-right:20px;margin-left:0 !important;" src="<?php echo YIKES_MC_URL . 'includes/images/Welcome_Page/glotpress-translation-percentage.png'; ?>" alt="<?php _e( 'GlotPress Localized Precentage', 'yikes-inc-easy-mailchimp-extender' ); ?>" class="yikes-easy-mc-feature-image yikes-easy-mc-whats-new-section-image">
+
+		<p><?php _e( "Since the initial release of this Easy Forms for MailChimp by YIKES we've been tirelessly working on putting together a collection of help articles ranging from troubleshooting tips to customization options. We've also included a collection of code snippets from some of our most widely asked questions. Now you can access some of our most popular articles right from within the plugin.",  'yikes-inc-easy-mailchimp-extender' ); ?></p>
+				
 		<p><h4><a href="<?php echo esc_url_raw( admin_url( 'admin.php?page=yikes-inc-easy-mailchimp-support#knowledge-base-articles' ) ); ?>" title="<?php _e( 'Support | Knowledge Base' , 'yikes-inc-easy-mailchimp-extender' ); ?>"><?php _e( 'Support → Knowledge Base Articles', 'yikes-inc-easy-mailchimp-extender' ); ?></a></h4></p>
-
-	</div>
-	
-	<hr />
-	
-	<div class="feature-section">
 		
-		<h2><?php _e( 'Form Setting Toggles' , 'yikes-inc-easy-mailchimp-extender' ); ?></h2>
+		<p>&nbsp;</p>
 		
-		<img style="float:right;" src="<?php echo YIKES_MC_URL . 'includes/images/Welcome_Page/optin-settings.png'; ?>" alt="<?php _e( 'Form Toggles', 'yikes-inc-easy-mailchimp-extender' ); ?>" class="yikes-easy-mc-feature-image yikes-easy-mc-whats-new-section-image">
-
-		<p><?php _e( "Now you can toggle single/double optins, trigger sending welcome emails or updating an existing subscriber on a per form basis and directly from within the plugin!",  'yikes-inc-easy-mailchimp-extender' ); ?></p>
+		<p><?php _e( "With a ton of help from the WordPress community, we've been able to support 7 international languages. We're always looking for additional translators, so if you are multi-lingual and can help translate things - hop into glotpress and help contribute to future development of this plugin.", 'yikes-inc-easy-mailchimp-extender' ); ?></p>
 		
-		<p><?php _e( "Along with the opt-in toggles, you have the ability to toggle ajax form submissions, redirecting users after a successful sign up and more!",  'yikes-inc-easy-mailchimp-extender' ); ?></p>
+		<p><h4><a href="<?php echo esc_url_raw( 'https://translate.wordpress.org/projects/wp-plugins/yikes-inc-easy-mailchimp-extender' ); ?>" target="_blank"title="<?php _e( 'Easy Forms for MailChimp | GlotPress' , 'yikes-inc-easy-mailchimp-extender' ); ?>"><?php _e( 'GlotPress', 'yikes-inc-easy-mailchimp-extender' ); ?></a></h4></p>
 
+		
 	</div>
 	
 	<hr />
@@ -67,7 +63,7 @@
 		
 		<img src="<?php echo YIKES_MC_URL . 'includes/images/Welcome_Page/settings-page.png'; ?>" alt="<?php _e( 'Settings Page', 'yikes-inc-easy-mailchimp-extender' ); ?>" class="yikes-easy-mc-feature-image yikes-easy-mc-whats-new-section-image">
 
-		<p><?php _e( "We've re-laid out the settings page to allow for a more intuitive workflow. All of your settings are neatly tucked away in their respective setting tab.",  'yikes-inc-easy-mailchimp-extender' ); ?></p>
+		<p style="float:left; padding-left:15px;margin-top:5px;"><?php _e( "We've re-laid out the settings page to allow for a more intuitive workflow. All of your settings are neatly tucked away in their respective setting tab.",  'yikes-inc-easy-mailchimp-extender' ); ?></p>
 		
 	</div>
 	
