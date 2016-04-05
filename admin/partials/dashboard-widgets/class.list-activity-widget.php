fd0046dc9b6450cbc6f0f49f13d369b2e30b9839
Evan Herman <evan@yikesinc.com>
2016-04-05 18:23:14 -0400

    exclude admin widgets for non-admins

--- admin/partials/dashboard-widgets/class.list-activity-widget.php
+++ admin/partials/dashboard-widgets/class.list-activity-widget.php
@@ -1,21 +1,19 @@
-<?php 
+<?php
 	/*
 	* Our ajax is processed inside of class.ajax.php
 	*/
 	class YIKES_Inc_Easy_MailChimp_Dashboard_Widgets
 	{
-			
+
 		// Construction
-		public function __construct() {		
+		public function __construct() {
 			if( get_option( 'yikes-mc-api-key' , '' ) != '' && get_option( 'yikes-mc-api-validation' , 'invalid_api_key' ) == 'valid_api_key' ) {
 				// hook in and display our list stats dashboard widget
-				add_action( 'wp_dashboard_setup', array( $this , 'yks_mc_add_chimp_chatter_dashboard_widget' ) , 10 );
-				// hook in and display our chimp chatter dashboard widget
 				add_action( 'wp_dashboard_setup', array( $this , 'yks_mc_add_chimp_chatter_dashboard_widget' ) , 10 );
 			}
 			add_action( 'admin_enqueue_scripts' , array( $this, 'enqueue_dashboard_widget_script' ) );
 		}
-		
+
 		// enqueue our JS file on the main dashboard page
 		function enqueue_dashboard_widget_script( $hook ) {
 			if( 'index.php' == $hook ) { // default 'dashboard' page
@@ -29,7 +27,7 @@
 				wp_enqueue_script( 'yikes-easy-mc-dashboard-widget-script' );
 			}
 		}
-				
+
 		/*******************************************************
 		Custom Dashboard MailChimp Account Activity Widget
 		********************************************************/
@@ -39,20 +37,23 @@
 		 * This function is hooked into the 'wp_dashboard_setup' action below.
 		 */
 		function yks_mc_add_chimp_chatter_dashboard_widget() {
-			/* List Stats Dashboard Widget */
-			wp_add_dashboard_widget(
-				 'yikes_easy_mc_list_stats_widget',         // Widget slug.
-				 __( 'MailChimp List Stats', 'yikes-inc-easy-mailchimp-extender' ),         // Title.
-				array( $this , 'list_stats_dashboard_widget' ) // Display function.
-			);	
-			/* Chimp Chatter Dashboard Widget */
-			wp_add_dashboard_widget(
-				 'yikes_easy_mc_account_activity_widget',         // Widget slug.
-				 __( 'MailChimp Account Activity', 'yikes-inc-easy-mailchimp-extender' ),         // Title.
-				array( $this , 'account_activity_dashboard_widget' ) // Display function.
-			);		
+			// If the current user is not an admin, abort
+			if ( apply_filters( 'yikes-mailchimp-user-role-access', current_user_can( 'manage_options' ) ) ) {
+				/* List Stats Dashboard Widget */
+				wp_add_dashboard_widget(
+					 'yikes_easy_mc_list_stats_widget',         // Widget slug.
+					 __( 'MailChimp List Stats', 'yikes-inc-easy-mailchimp-extender' ),         // Title.
+					array( $this , 'list_stats_dashboard_widget' ) // Display function.
+				);
+				/* Chimp Chatter Dashboard Widget */
+				wp_add_dashboard_widget(
+					 'yikes_easy_mc_account_activity_widget',         // Widget slug.
+					 __( 'MailChimp Account Activity', 'yikes-inc-easy-mailchimp-extender' ),         // Title.
+					array( $this , 'account_activity_dashboard_widget' ) // Display function.
+				);
+			}
 		}
-					
+
 		/**
 		* Create the function to output our list stats dashboard widget
 		*/
@@ -65,16 +66,16 @@
 					if( $dash_position !== false ) {
 						$api_endpoint = 'https://' . substr( $api_key, $dash_position + 1 ) . '.api.mailchimp.com/2.0/lists/list.json';
 					}
-					$list_data = wp_remote_post( $api_endpoint, array( 
-						'body' => array( 
-							'apikey' => $api_key, 
+					$list_data = wp_remote_post( $api_endpoint, array(
+						'body' => array(
+							'apikey' => $api_key,
 							'limit' => 100
 						),
 						'timeout' => 10,
 						'sslverify' => apply_filters( 'yikes-mailchimp-sslverify', true )
 					) );
 					$list_data = json_decode( wp_remote_retrieve_body( $list_data ), true );
-					if( isset( $list_data['error'] ) ) {	
+					if( isset( $list_data['error'] ) ) {
 						if( WP_DEBUG || get_option( 'yikes-mailchimp-debug-status' , '' ) == '1' ) {
 							require_once YIKES_MC_PATH . 'includes/error_log/class-yikes-inc-easy-mailchimp-error-logging.php';
 							$error_logging = new Yikes_Inc_Easy_Mailchimp_Error_Logging();
@@ -85,13 +86,13 @@
 						set_transient( 'yikes-easy-mailchimp-list-data', $list_data, 1 * HOUR_IN_SECONDS );
 					}
 				}
-			?>					
+			?>
 				<!-- Dropdown to Change the list -->
 				<?php if( ! empty( $list_data['data'] ) ) {
 					?><section class="inside-widget yikes-dashboard-widget-section">
 							<strong class="select-list-title"><?php _e( 'Select a list' , 'yikes-inc-easy-mailchimp-extender' ) ?>:</strong>
 							<select id="yikes-easy-mc-dashboard-change-list" class="widefat">
-								<?php 
+								<?php
 									foreach( $list_data['data'] as $list ) {
 										?><option val="<?php echo $list['id']; ?>"><?php echo $list['name']; ?></option><?php
 									}
@@ -101,9 +102,9 @@
 					</section>
 					<!-- display stats here! -->
 					<section id="yikes-easy-mc-dashboard-widget-stats">
-						<?php 
+						<?php
 							if( !empty( $list_data['data'] ) ) {
-								include_once( YIKES_MC_PATH . 'admin/partials/dashboard-widgets/templates/stats-list-template.php' ); 
+								include_once( YIKES_MC_PATH . 'admin/partials/dashboard-widgets/templates/stats-list-template.php' );
 							}
 						?>
 					</section>
@@ -112,29 +113,29 @@
 						<p class="no-lists-error"><?php _e( "Whoops, you don't have any lists set up. Head over to MailChimp to set up lists." , 'yikes-inc-easy-mailchimp-extender' ); ?></p>
 					</section>
 				<?php }
-		} 
-		
+		}
+
 		/**
 		* Create the function to output our account activity dashboard widget
 		*/
 		function account_activity_dashboard_widget() {
 				// Get our list data!
 				// Check for a transient, if not - set one up for one hour
-				if ( false === ( $account_activity = get_transient( 'yikes-easy-mailchimp-account-activity' ) ) ) {		
+				if ( false === ( $account_activity = get_transient( 'yikes-easy-mailchimp-account-activity' ) ) ) {
 					$api_key = trim( get_option( 'yikes-mc-api-key' , '' ) );
 					$dash_position = strpos( $api_key, '-' );
 					if( $dash_position !== false ) {
 						$api_endpoint = 'https://' . substr( $api_key, $dash_position + 1 ) . '.api.mailchimp.com/2.0/helper/chimp-chatter.json';
 					}
-					$account_activity = wp_remote_post( $api_endpoint, array( 
-						'body' => array( 
+					$account_activity = wp_remote_post( $api_endpoint, array(
+						'body' => array(
 							'apikey' => $api_key
 						),
 						'timeout' => 10,
 						'sslverify' => apply_filters( 'yikes-mailchimp-sslverify', true )
 					) );
 					$account_activity = json_decode( wp_remote_retrieve_body( $account_activity ), true );
-					if( isset( $account_activity['error'] ) ) {	
+					if( isset( $account_activity['error'] ) ) {
 						if( WP_DEBUG || get_option( 'yikes-mailchimp-debug-status' , '' ) == '1' ) {
 							require_once YIKES_MC_PATH . 'includes/error_log/class-yikes-inc-easy-mailchimp-error-logging.php';
 							$error_logging = new Yikes_Inc_Easy_Mailchimp_Error_Logging();
@@ -146,10 +147,10 @@
 					}
 				}
 				if( ! empty( $account_activity ) ) {
-					include_once( YIKES_MC_PATH . 'admin/partials/dashboard-widgets/templates/account-activity-template.php' ); 
+					include_once( YIKES_MC_PATH . 'admin/partials/dashboard-widgets/templates/account-activity-template.php' );
 				}
-		} 
-		
+		}
+
 	} // end class
 	new YIKES_Inc_Easy_MailChimp_Dashboard_Widgets();
 ?>
