6410253c049f06ee02cd6edc3da37986396f28cc
Jeremy Pry <jeremy.pry@gmail.com>
2016-10-12 13:38:51 -0400

    One more place to remove json_decode()

--- admin/partials/helpers/fields/yikes-mailchimp-checkbox-field.php
+++ admin/partials/helpers/fields/yikes-mailchimp-checkbox-field.php
@@ -1,11 +1,11 @@
 <?php 
-	/*
-	* 	Standard Checkbox Input Field
-	*
-	*	For help on using, see our documentation [https://yikesplugins.com/support/knowledge-base/product/easy-forms-for-mailchimp/]
-	* 	@since 6.0
-	*/
-	$field_data = json_decode( $form_data['custom_fields'] , true ); 
+/*
+* 	Standard Checkbox Input Field
+*
+*	For help on using, see our documentation [https://yikesplugins.com/support/knowledge-base/product/easy-forms-for-mailchimp/]
+* 	@since 6.0
+*/
+$field_data = $form_data['custom_fields'];
 ?>
 <label class="custom-field-section">
 	<strong><?php echo $field['label']; ?></strong>
