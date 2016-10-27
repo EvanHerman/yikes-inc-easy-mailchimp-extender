1f96221af5ced59f81d610e74f48d0206eddac9a
Evan Herman <Evan@Evans-MacBook-Pro.local>
2016-10-27 08:52:39 -0400

    Fix path to kb article links on welcome page.

--- admin/partials/welcome-page/welcome-sections/knowledge-base-section.php
+++ admin/partials/welcome-page/welcome-sections/knowledge-base-section.php
@@ -1,22 +1,17 @@
 <?php
-	// get the YIKES knowledge base articles
-	/* if ( false === ( $knowledge_base_articles = get_transient( 'yikes-mailchimp-knowledge-base-transient' ) ) ) {
-		// It wasn't there, so regenerate the data and save the transient
-		$knowledge_base_articles = wp_remote_get( 'https://yikesplugins.com/feed/?post_type=kbe_knowledgebase', array( 'timeout' => 120 ) );
-		$knowledge_base_articles = json_decode( $contributors['body'] , true );
-		set_transient( 'yikes-mailchimp-knowledge-base-transient', $contributors, 60*60*12 );
-	} */
-	
-	
+/**
+ * Welcome Page - Knowledge Base Links
+ */
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 ?>
 <div class="wrap about-wrap">
-	
+
 	<div class="feature-section">
-						
+
 		<div id="kb-container">
-			<?php include_once( YIKES_MC_PATH . 'admin/partials/helpers/knowledge-base-articles-RSS.php' ); ?>
+			<?php include_once( YIKES_MC_PATH . 'admin/partials/helpers/knowledge-base-article-links.php' ); ?>
 		</div>
-			
+
 	</div>
-	
+
 </div>
