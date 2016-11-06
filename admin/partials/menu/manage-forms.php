b3355455e3098190fae835148205f5b3fd5dfbd9
Jeremy Pry <jeremy.pry@gmail.com>
2016-11-06 16:42:41 -0500

    Update the checking for the WP_Error

--- admin/partials/menu/manage-forms.php
+++ admin/partials/menu/manage-forms.php
@@ -22,19 +22,26 @@ if( $this->is_user_mc_api_valid_form( false ) == 'valid' ) {
 			'timeout' => 10,
 			'sslverify' => apply_filters( 'yikes-mailchimp-sslverify', true )
 		) );
+
+		if ( is_wp_error( $list_data ) ) {
+			if ( WP_DEBUG || get_option( 'yikes-mailchimp-debug-status', '' ) == '1' ) {
+				$error_logging = new Yikes_Inc_Easy_Mailchimp_Error_Logging();
+				$error_logging->yikes_easy_mailchimp_write_to_error_log( $list_data->get_error_message(), __( "Get Account Lists", 'yikes-inc-easy-mailchimp-extender' ), "Manage Forms Page" );
+			}
+			$list_data = null;
+		}
+
 		$list_data = json_decode( wp_remote_retrieve_body( $list_data ), true );
 		if ( isset( $list_data['error'] ) ) {
 			if ( WP_DEBUG || get_option( 'yikes-mailchimp-debug-status', '' ) == '1' ) {
 				$error_logging = new Yikes_Inc_Easy_Mailchimp_Error_Logging();
 				$error_logging->yikes_easy_mailchimp_write_to_error_log( $list_data['error'], __( "Get Account Lists", 'yikes-inc-easy-mailchimp-extender' ), "Manage Forms Page" );
 			}
-		} elseif ( is_wp_error( $list_data ) ) {
-			if ( WP_DEBUG || get_option( 'yikes-mailchimp-debug-status', '' ) == '1' ) {
-				$error_logging = new Yikes_Inc_Easy_Mailchimp_Error_Logging();
-				$error_logging->yikes_easy_mailchimp_write_to_error_log( $list_data->get_error_message() , __( "Get Account Lists", 'yikes-inc-easy-mailchimp-extender' ), "Manage Forms Page" );
-			}
-		} else {
-			// set our transient
+			$list_data = null;
+		}
+
+		// Maybe set our transient
+		if ( null !== $list_data ) {
 			set_transient( 'yikes-easy-mailchimp-list-data', $list_data, HOUR_IN_SECONDS );
 		}
 	}
