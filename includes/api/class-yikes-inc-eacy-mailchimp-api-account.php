98d879dcd60f9ccf91c649731f9abf31116b1e6d
Jeremy Pry <jeremy.pry@gmail.com>
2016-12-11 18:56:56 -0500

    Store transient key in a variable

--- includes/api/class-yikes-inc-eacy-mailchimp-api-account.php
+++ includes/api/class-yikes-inc-eacy-mailchimp-api-account.php
@@ -15,14 +15,15 @@ class Yikes_Inc_Easy_MailChimp_API_Account extends Yikes_Inc_Easy_MailChimp_API_
 	 * @return array|WP_Error
 	 */
 	public function get_account( $use_transient = true ) {
-		$transient = get_transient( 'yikes_eme_account' );
+		$transient_key = 'yikes_eme_account';
+		$transient     = get_transient( $transient_key );
 		if ( false !== $transient && $use_transient ) {
 			return $transient;
 		}
 
 		$response = $this->maybe_return_error( $this->get_from_api() );
 		if ( ! is_wp_error( $response ) ) {
-			set_transient( 'yikes_eme_account', $response, HOUR_IN_SECONDS );
+			set_transient( $transient_key, $response, HOUR_IN_SECONDS );
 		}
 
 		return $response;
