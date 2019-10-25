ebe509cb277dfa9d560b836e346aa1301ad77af6
freddiemixell <fmixell@gmail.com>
2019-10-25 01:33:43 -0400

    double optin tags

--- public/partials/shortcodes/process/process_form_submission.php
+++ public/partials/shortcodes/process/process_form_submission.php
@@ -248,6 +248,10 @@ if ( is_wp_error( $member_exists ) || $double_optin_resubscribe === true ) {
 $member_data = apply_filters( 'yikes-mailchimp-filter-subscribe-request', $member_data, $form_id );
 $member_data = apply_filters( "yikes-mailchimp-filter-subscribe-request-{$form_id}", $member_data, $form_id );
 
+foreach( $form_data['tags'] as $tag ) {
+	$member_data['tags'][] = $tag['name'];
+}
+
 // Send the API request to create a new subscriber! (Or update an existing one)
 $subscribe_response = $list_handler->member_subscribe( $list_id, md5( $sanitized_email ), $member_data );
 
@@ -257,9 +261,6 @@ $subscribe_response = $list_handler->member_subscribe( $list_id, md5( $sanitized
 if ( is_wp_error( $subscribe_response ) ) {
 	$success_array = $submission_handler->handle_submission_response_error( $subscribe_response, $form_fields );
 } else {
-
-	// Check if we have any tags to add.
-	$tags_response = $submission_handler->maybe_add_tags( $form_data, $data );
 
 	$submission_handler->handle_submission_response_success( $submission_settings, array(), $merge_variables, $notifications, $optin_settings, $new_subscriber );
 }
