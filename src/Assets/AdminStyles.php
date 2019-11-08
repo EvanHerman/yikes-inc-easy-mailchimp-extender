6600c4afadd7c54f72b9103f266c02b68ff85cbb
freddiemixell <fmixell@gmail.com>
2019-11-08 17:10:32 -0500

    admin assets

---
+++ src/Assets/AdminStyles.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * YIKES Inc. Easy Mailchimp Forms Plugin.
+ *
+ * @package   Yikes\EasyForms
+ * @author    Freddie Mixell
+ * @license   GPL2
+ */
+
+namespace YIKES\EasyForms\Assets;
+
+use Yikes\EasyForms\Service;
+
+/**
+ * Class AdminStyles
+ *
+ * Handles registration of stylesheet for the entire admin area.
+ *
+ * @since   %VERSION%
+ * @package Yikes\EasyForms
+ */
+class AdminStyles implements Service, AssetsAware {
+
+	use AssetsAwareness;
+
+	/**
+	 * Register the current Registerable.
+	 *
+	 * @since %VERSION%
+	 */
+	public function register() {
+		$this->register_assets();
+
+		add_action( 'admin_enqueue_scripts', function() {
+			$this->enqueue_assets();
+		} );
+	}
+
+	/**
+	 * Load asset objects for use.
+	 *
+	 * @since %VERSION%
+	 */
+	protected function load_assets() {
+		$this->assets = [
+			new StyleAsset( 'easy-forms-admin-css', 'assets/css/admin' ),
+		];
+	}
+}
