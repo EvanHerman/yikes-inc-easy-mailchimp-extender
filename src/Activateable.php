eb8abc02feace589beb48c3d2a6954176434a726
freddiemixell <fmixell@gmail.com>
2019-11-08 16:47:25 -0500

    interface for activating

---
+++ src/Activateable.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * YIKES Inc. Easy Forms.
+ *
+ * @package Yikes\EasyForms
+ * @author  Freddie Mixell
+ * @license GPL2
+ */
+
+namespace Yikes\EasyForms;
+
+/**
+ * Interface Activateable
+ *
+ * @since   %VERSION%
+ * @package Yikes\EasyForms
+ */
+interface Activateable {
+
+	/**
+	 * Activate the service.
+	 *
+	 * @since %VERSION%
+	 */
+	public function activate();
+}
