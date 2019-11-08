a03e0fef485a2c6514023bc909315d4a194a935f
freddiemixell <fmixell@gmail.com>
2019-11-08 17:09:32 -0500

    interface for registering assets

---
+++ src/Assets/Asset.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * YIKES Inc. Easy Mailchimp Forms Plugin.
+ *
+ * @package   Yikes\EasyForms
+ * @author    Freddie Mixell
+ * @license   GPL2
+ */
+
+namespace YIKES\EasyForms\Assets;
+
+use Yikes\EasyForms\Registerable;
+
+/**
+ * Interface Asset.
+ *
+ * @since   %VERSION%
+ *
+ * @package Yikes\EasyForms\Assets
+ * @author  Freddie Mixell
+ */
+interface Asset extends Registerable {
+
+	/**
+	 * Enqueue the asset.
+	 *
+	 * @since %VERSION%
+	 */
+	public function enqueue();
+
+	/**
+	 * Dequeue the asset.
+	 *
+	 * @since %VERSION%
+	 */
+	public function dequeue();
+
+	/**
+	 * Get the handle of the asset.
+	 *
+	 * @since %VERSION%
+	 *
+	 * @return string
+	 */
+	public function get_handle();
+}
