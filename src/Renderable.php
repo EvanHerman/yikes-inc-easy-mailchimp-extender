884a19ee4b79869ab8bd54c7725ce69661ba08e4
freddiemixell <fmixell@gmail.com>
2019-11-08 16:48:57 -0500

    interface to render

---
+++ src/Renderable.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * YIKES Inc. Easy Forms.
+ *
+ * @package Yikes\EasyForms
+ * @author  Freddie Mixell
+ * @license GPL2
+ */
+
+namespace Yikes\EasyForms;
+
+/**
+ * Interface Renderable.
+ *
+ * An object that can be `render()`ed.
+ *
+ * @since   %VERSION%
+ *
+ * @package Yikes\EasyForms
+ * @author  Freddie Mixell
+ */
+interface Renderable {
+	/**
+	 * Render the current Renderable.
+	 *
+	 * @since %VERSION%
+	 *
+	 * @param array $context Context in which to render.
+	 *
+	 * @return string Rendered HTML.
+	 */
+	public function render( array $context = [] );
+}
