7c07537d126354198debb584b8a83ed866502bba
freddiemixell <fmixell@gmail.com>
2019-11-08 16:41:31 -0500

    Container to hold registered services

---
+++ src/Container.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * YIKES Inc. Easy Forms.
+ *
+ * @package Yikes\EasyForms
+ * @author  Freddie Mixell
+ * @license GPL2
+ */
+
+namespace Yikes\EasyForms;
+
+/**
+ * Class Container
+ *
+ * @since   %VERSION%
+ * @package Yikes\EasyForms
+ */
+final class Container {
+
+	/**
+	 * The registered services for the container.
+	 *
+	 * @since %VERSION%
+	 * @var array
+	 */
+	protected $services = [];
+
+	/**
+	 * Container constructor.
+	 *
+	 * @param array $services Services to register with the container.
+	 */
+	public function __construct( array $services = [] ) {
+		$this->services = $services ?: [];
+	}
+
+	/**
+	 * Get the services from the container.
+	 *
+	 * @since %VERSION%
+	 * @return array
+	 */
+	public function get_services() {
+		return $this->services;
+	}
+
+	/**
+	 * Add a service to the container.
+	 *
+	 * @since %VERSION%
+	 *
+	 * @param string $service Service class name.
+	 */
+	public function add_service( $service ) {
+		$this->services[ $service ] = true;
+	}
+
+	/**
+	 * Remove a service from the container.
+	 *
+	 * @since %VERSION%
+	 *
+	 * @param string $service Service class name.
+	 */
+	public function remove_service( $service ) {
+		unset( $this->services[ $service ] );
+	}
+}
