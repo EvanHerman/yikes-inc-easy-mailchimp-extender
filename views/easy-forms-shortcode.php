1c8a467977c62896a7b78991ae86ae3ead5d32c4
freddiemixell <fmixell@gmail.com>
2019-11-09 13:39:54 -0500

    abstracting away pieces of the form

--- views/easy-forms-shortcode.php
+++ views/easy-forms-shortcode.php
@@ -22,50 +22,63 @@ if ( ! defined( 'ABSPATH' ) ) {
 $form_data = $this->form_data;
 
 $form_settings = $this->form_settings;
-// Set up the classes we'll use for the form and the individual fields.
-$form_classes = $form_settings['yikes-easy-mc-form-class-names'];
 
-$inline_form           = $form_settings['yikes-easy-mc-inline-form'];
-$submit_button_type    = $form_settings['yikes-easy-mc-submit-button-type'];
-$submit_button_text    = esc_attr( $form_settings['yikes-easy-mc-submit-button-text'] );
-$submit_button_image   = esc_url( $form_settings['yikes-easy-mc-submit-button-image'] );
-$submit_button_classes = esc_attr( $form_settings['yikes-easy-mc-submit-button-classes'] );
-
-// schedules
-$form_schedule_state  = $form_settings['yikes-easy-mc-form-schedule'];
-$form_schedule_start  = $form_settings['yikes-easy-mc-form-restriction-start'];
-$form_schedule_end    = $form_settings['yikes-easy-mc-form-restriction-end'];
-$form_pending_message = $form_settings['yikes-easy-mc-form-restriction-pending-message'];
-$form_expired_message = $form_settings['yikes-easy-mc-form-restriction-expired-message'];
-
-// register required
-$form_login_required = $form_settings['yikes-easy-mc-form-login-required'];
-$form_login_message  = $form_settings['yikes-easy-mc-form-restriction-login-message'];
+$form->login_required();
 
 ?>
-<?php if ( $form->has_errors() ) : ?>
-	<div class="emf-form-errors">
-		<?php esc_html_e( 'Your form has errors. Please correct the errors below before resubmitting.', 'easy-forms-text-domain' ); ?>
-	</div>
-<?php endif; ?>
-<form method="POST"
-	id="<?php echo esc_attr( $this->form_id ); ?>"
-	class="<?php echo esc_attr( join( ' ', $form_classes ) ); ?>"
+<?php 
+
+$form->form_schedule();
+
+ob_start();
+?>
+<section
+	id="yikes-mailchimp-container-<?php echo $this->form_id; ?>"
+	class="yikes-mailchimp-container yikes-mailchimp-container-<?php echo $this->form_id; ?> <?php echo apply_filters( 'yikes-mailchimp-form-container-class', '', $this->form_id ); ?>"
 >
-	<?php $form->render(); ?>
-	<button class="emf-submit" type="submit" name="emf_submit">
-		<?php esc_html_e( 'Submit Form', 'easy-forms-text-domain' ); ?>
-	</button>
-</form>
+<?php
+/*
+*  pre-form action hooks
+*  check readme for usage examples
+*/
+do_action( 'yikes-mailchimp-before-form', $this->form_id, $this->form_data );
+
+?>
+	<!-- Form Title -->
+	<?php $this->form_title(); ?>
+
+	<!-- Form Description -->
+	<?php $this->form_description(); ?>
+
+	<form method="POST"
+		id="<?php $form->form_id_prop(); ?>"
+		class="<?php $form->form_classes(); ?>"
+	>
+		<!-- Form Fields -->
+		<?php $form->render(); ?>
+		<?php
+			// Show Recaptcha If Enabled.
+			$form->recaptcha();
+		?>
+		<!-- Submit Button -->
+		<?php $form->submit_button(); ?>
+	</form>
+
 
 <?php
+// Form Edit Link
+$form->edit_form_link();
+?>
 
-// Display an edit link for the application.
-$form_edit_link = get_edit_post_link( $this->form_id, 'link' );
-if ( $form_edit_link ) {
-	printf(
-		'<br/><p><a href="%1$s">%2$s</a></p>',
-		esc_url( $form_edit_link ),
-		esc_html__( 'Edit Form', 'easy-forms-text-domain' )
-	);
-}
+<?php
+/*
+*  post-form action hooks
+*  check readme for usage examples
+*/
+do_action( 'yikes-mailchimp-after-form', $this->form_id, $this->form_data );
+
+?>
+</section>
+<?php
+
+return ob_get_clean();
