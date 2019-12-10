04973e004f3f8065e3dd349435601ef86e4f362d
freddiemixell <fmixell@gmail.com>
2019-12-10 09:43:34 -0500

    basic field types

--- src/Field/Types.php
+++ src/Field/Types.php
@@ -18,24 +18,7 @@ namespace YIKES\EasyForms\Field;
  * @package YIKES\EasyForms
  */
 interface Types {
-
-	const TEXT           = Text::class;
-	const TEXTAREA       = Textarea::class;
-	const EMAIL          = Email::class;
-	const HIDDEN         = Hidden::class;
-	const NUMBER         = Number::class;
-	const PHONE          = Phone::class;
-	const DATE           = Date::class;
-	const CHECKBOX       = Checkbox::class;
-	const SELECT         = Select::class;
-	const ADDRESS        = Address::class;
-	const POSTAL_CODE    = PostalCode::class;
-	const YEAR           = Year::class;
-	const SCHOOLING      = Schooling::class;
-	const CERTIFICATIONS = Certifications::class;
-	const EXPERIENCE     = Experience::class;
-	const VOLUNTEER      = Volunteer::class;
-	const SKILLS         = Skills::class;
-	const LANGUAGES      = Languages::class;
-	const WYSIWYG        = WYSIWYG::class;
+	const TEXT   = Text::class;
+	const EMAIL  = Email::class;
+	const HIDDEN = Hidden::class;
 }
